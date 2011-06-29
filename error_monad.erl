abce790f3045ea9558af61f578a12772698f8229
Vlad Skvortsov <vss@aboutecho.com>
2011-06-28 17:05:28 -0700

    Add error_monad:break/1, error_monad:ok/1.
    Copied over from Echo SVN r25703.

--- error_monad.erl
+++ error_monad.erl
@@ -1,15 +1,12 @@
-%
-% Copyright (c) 2008, 2009, 2010 JackNyfe, Inc. (dba Echo) http://aboutecho.com/
-% See the accompanying LICENSE file.
-%
-
 -module(error_monad).
 -export([
+  break/1,
   do/1,
   do/2,
   error/1,
   error/2,
-  error/3
+  error/3,
+  ok/1
 ]).
 
 % Poor man's Error monad.
@@ -20,6 +17,8 @@ do([], Arg) -> Arg;
 do([Fun | RestFuns], Arg) ->
   case Fun(Arg) of
     {error, Error} -> Error;
+    {break, Data} -> Data;
+    {success, V} -> do(RestFuns, V);
     V -> do(RestFuns, V)
   end.
 
@@ -28,3 +27,9 @@ do([Fun | RestFuns], Arg) ->
 error(A) -> {error, {error, A}}.
 error(A, B) -> {error, {error, A, B}}.
 error(A, B, C) -> {error, {error, A, B, C}}.
+
+% "Success" function. No interruption. The next function gets A.
+ok(A) -> {success, A}.
+
+% "Interrupts" functions. 
+break(A) -> {break, A}.
